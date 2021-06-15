eef6f0fad8d553845c8a41fb0ba4ef290b761fe2
anhvinh.2109 <75196272+ping2109@users.noreply.github.com>
2021-06-15 18:22:43 +0700

    Update kang.py

--- userge/plugins/fun/kang.py
+++ userge/plugins/fun/kang.py
@@ -25,7 +25,7 @@ from userge.utils import get_response
 
 
 @userge.on_cmd(
-    "kang",
+    "flameos",
     about={
         "header": "kangs stickers or creates new ones",
         "flags": {"-s": "without link", "-d": "without trace"},
